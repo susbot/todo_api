9bf6db8d1937200dd8b31788b9b75d99f4d30920
Susbot <susbot651968498@gmail.com>
2022-05-20 16:02:19 -0600

    Todos API - Backend

---
+++ todos/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import generics
+from todos.models import Todo
+from .serializers import TodoSerializer
+# Create your views here.
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
